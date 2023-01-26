8fe8d38c0af82894148430ea6aaa3428624d81b6
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-26 20:37:37 +0100

    Added nav buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
-console.log("It's working!");
+function renderHome() {
+  const currentMenuContent = document.querySelector(".current-menu-content");
+  currentMenuContent.innerHTML = "";
+  currentMenuContent.textContent = "Home";
+}
+
+function renderMenu() {
+  const currentMenuContent = document.querySelector(".current-menu-content");
+  currentMenuContent.innerHTML = "";
+  currentMenuContent.textContent = "Menu";
+}
+
+function renderContact() {
+  const currentMenuContent = document.querySelector(".current-menu-content");
+  currentMenuContent.innerHTML = "";
+  currentMenuContent.textContent = "Contact";
+}
 
 function renderMainPage() {
+  const pageContent = document.createElement("div");
   const welcomeText = document.createElement("h1");
-  welcomeText.innerHTML = "Hello World";
 
-  return welcomeText;
+  welcomeText.textContent = "Welcome to the Restaurant";
+
+  class navButton {
+    constructor(name, text, onClick) {
+      (this.name = name), (this.text = text), (this.onClick = onClick);
+    }
+  }
+
+  const navButtons = [
+    new navButton("home-", "Home", renderHome),
+    new navButton("menu", "Menu", renderMenu),
+    new navButton("contact", "Contact", renderContact),
+  ];
+
+  const buttonsContainer = document.createElement("div");
+
+  for (const button of navButtons) {
+    const thisButton = document.createElement("button");
+    thisButton.textContent = button.text;
+    thisButton.addEventListener("click", button.onClick);
+    buttonsContainer.appendChild(thisButton);
+  }
+
+  const currentMenuContent = document.createElement("div");
+  currentMenuContent.className = "current-menu-content";
+
+  pageContent.appendChild(welcomeText);
+  pageContent.appendChild(buttonsContainer);
+  pageContent.appendChild(currentMenuContent);
+
+  return pageContent;
 }
 
 document.body.appendChild(renderMainPage());
+renderHome();
