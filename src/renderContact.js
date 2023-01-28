c82d728fe932ffc2d8727b328f2fde91f310319b
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-28 18:49:27 +0100

    Contact page ready

--- src/renderContact.js
+++ src/renderContact.js
@@ -6,6 +6,24 @@ export default function renderContact() {
   const currentMenuContent = document.querySelector(".current-menu-content");
 
   currentMenuContent.innerHTML = "";
+
+  const contactContainer = document.createElement("div");
+  contactContainer.className = "contact-container";
+
+  const address = document.createElement("h3");
+  address.textContent =
+    "23 Oceanfront Avenue, Seaside City, Coastal County, 12345";
+
+  const phone = document.createElement("h4");
+  phone.textContent = "Phone number: 555-555-5555";
+
+  const email = document.createElement("p");
+  email.textContent = "Email: info@surfandturfbistro.com";
+
+  contactContainer.appendChild(address, phone, email);
+  contactContainer.appendChild(phone);
+  contactContainer.appendChild(email);
+  currentMenuContent.appendChild(contactContainer);
 }
 
 // "123 Oceanfront Avenue, Seaside City, Coastal County, 12345"
