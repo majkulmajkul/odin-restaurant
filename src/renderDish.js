2fc42a0805be998f5a9751352b045f5359093fe3
Miklós Joánovits <72404757+majkulmajkul@users.noreply.github.com>
2023-01-28 18:30:55 +0100

    Further styles to Dishes

--- src/renderDish.js
+++ src/renderDish.js
@@ -4,12 +4,14 @@ export default function renderDish(dishObject) {
 
   const dishCardTitle = document.createElement("h3");
   dishCardTitle.textContent = dishObject.name;
+  dishCardTitle.className = "dish-card-title";
 
   const dishCardText = document.createElement("p");
   dishCardText.textContent = dishObject.description;
 
   const dishCardPrice = document.createElement("h4");
   dishCardPrice.textContent = dishObject.price;
+  dishCardPrice.className = "dish-card-price";
 
   dishCard.appendChild(dishCardTitle);
   dishCard.appendChild(dishCardText);
